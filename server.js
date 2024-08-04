6d41a6a609a046d1905d133d265d5b4e6fac4da4
kubekthecreator <50717269+kubekthecreator@users.noreply.github.com>
2024-08-04 16:19:50 +0200

    fix railway by santander stażysta juniorek 3

--- server.js
+++ server.js
@@ -8,13 +8,25 @@ const target = process.env.BACKEND_URL || DEFAULT_TARGET;
 const proxyConfig = {
   target: target,
   secure: true,
-  changeOrigin: true
+  changeOrigin: true,
+  protocolRewrite: 'https',
+  headers: {
+    'X-Forwarded-Proto': 'https',
+  },
 };
 
 const proxyPaths = ['/api', '/oauth2', '/login', '/assets'];
 
 proxyPaths.forEach(path => {
   app.use(path, createProxyMiddleware(proxyConfig));
+});
+
+// Middleware do przekierowania HTTP na HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  next();
 });
 
 app.use(express.static(__dirname + "/dist/airbnb-clone-front/browser/"));
