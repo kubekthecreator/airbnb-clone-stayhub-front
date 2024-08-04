d41a5cebcd05b6f1bb3a57ca3c947f8facd48298
kubekthecreator <50717269+kubekthecreator@users.noreply.github.com>
2024-08-04 14:37:29 +0200

    fix railway by santander stażysta juniorek 2

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: false,
   API_URL: "//localhost:4200/api",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
   API_URL: '//airbnb-clone-stayhub-front-production.up.railway.app/api',
 };
